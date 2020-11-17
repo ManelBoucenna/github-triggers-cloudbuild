6f68972cc5718701e52447142c380000dcf286b7
manalBM <74430468+manalBM@users.noreply.github.com>
2020-11-17 10:11:03 -0500

    Add files via upload

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,1 +1,27 @@
- 
+locals {
+  network = "${element(split("-", var.subnet), 0)}"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  project      = "${var.project}"
+  zone         = "us-west1-a"
+  name         = "${local.network}-vm-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    subnetwork = "${var.subnet}"
+
+    access_config {
+      # Include this section to give the VM an external ip address
+    }
+  }
+
+  # Apply the firewall rule to allow external IPs to access this instance
+  tags = ["vm-instance"]
+}
